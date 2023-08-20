23fd72994737374c099867924e57da3cf3a199b3
yonggyo1125 <yonggyo00@kakao.com>
2023-08-21 08:57:40 +0900

    파일 업로드 공통 기능 추가중

---
+++ src/main/java/com/foocmend/repositories/FileInfoRepository.java
@@ -0,0 +1,8 @@
+package com.foocmend.repositories;
+
+import com.foocmend.entities.FileInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface FileInfoRepository extends JpaRepository<FileInfo, Long>, QuerydslPredicateExecutor<FileInfo> {
+}
