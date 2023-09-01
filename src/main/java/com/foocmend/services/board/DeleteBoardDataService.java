850d7453ddf78a6e2c05d44932bb38def125c505
Jieun011211 <qhfltldkt@naver.com>
2023-09-01 16:09:42 +0900

    게시판 삭제 업데이트

--- src/main/java/com/foocmend/services/board/DeleteBoardDataService.java
+++ src/main/java/com/foocmend/services/board/DeleteBoardDataService.java
@@ -1,16 +1,26 @@
 package com.foocmend.services.board;
 
+import com.foocmend.commons.Utils;
+import com.foocmend.controllers.admin.BoardForm;
+import com.foocmend.controllers.admin.RestaurantForm;
+import com.foocmend.entities.Board;
 import com.foocmend.entities.BoardData;
+import com.foocmend.entities.Restaurant;
 import com.foocmend.repositories.BoardDataRepository;
+import com.foocmend.repositories.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class DeleteBoardDataService {
     private final BoardDataRepository repository;
+    private final BoardRepository boardRepository;
+    private final Utils utils;
 
     /**
      * 게시글 삭제
@@ -32,5 +42,22 @@ public class DeleteBoardDataService {
 
     public void delete(Long id) {
         delete(id, false);
+    }
+
+    public void delete(BoardForm form) {
+        List<Integer> chkNos = form.getChkNo();
+        List<Board> items = new ArrayList<>();
+        for (int chkNo : chkNos) {
+            Long bid = Long.valueOf(utils.getParam("bid_" + chkNo));
+            Board item = boardRepository.findById(String.valueOf(bid)).orElse(null);
+            if (item == null) {
+
+                continue;
+            }
+            items.add(item);
+        }
+
+        boardRepository.deleteAll(items);
+        boardRepository.flush();
     }
 }
