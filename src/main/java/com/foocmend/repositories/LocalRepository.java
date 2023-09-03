00ca273b5bda5cbfb305f7ec0eb6b5c677af01f6
“tlschdaud” <tlschdaud1@naver.com>
2023-09-03 16:36:58 +0900

    필요없는 파일 삭제.

--- src/main/java/com/foocmend/repositories/LocalRepository.java
+++
@@ -1,8 +0,0 @@
-package com.foocmend.repositories;
-
-
-import com.foocmend.entities.Local;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocalRepository extends JpaRepository<Local, String> {
-}
